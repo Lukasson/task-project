db857af701710ebaba20e69c311b7f8256596be1
“Jason <j.lukasson@gmail.com>
2014-09-22 10:48:09 -0500

    modified tasks_controller to include universal variables run test

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -77,8 +77,8 @@ class TasksController < ApplicationController
           :address              => 'smtp.gmail.com',
           :port                 => '587',
           :enable_starttls_auto => true,
-          :user_name            => 'taskmate.ocs@gmail.com',
-          :password             => 'derpderpderp',
+          :user_name            => ENV[:MAIL_ACCOUNT],
+          :password             => ENV[:MAIL_PASSWORD],
           :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
           :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
         }
